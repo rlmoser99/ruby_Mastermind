7ff11e108daa825f4605ddcadfd330504e8008c7
Rachel Moser <moser.rachel@gmail.com>
2020-04-03 10:46:57 -0500

    final game pieces and clues

--- main.rb
+++ main.rb
@@ -1,46 +1,37 @@
 class String
-  def black;          "\e[30m#{self}\e[0m" end
-  def red;            "\e[31m#{self}\e[0m" end
-  def green;          "\e[32m#{self}\e[0m" end
-  def brown;          "\e[33m#{self}\e[0m" end
-  def blue;           "\e[34m#{self}\e[0m" end
-  def magenta;        "\e[35m#{self}\e[0m" end
-  def cyan;           "\e[36m#{self}\e[0m" end
   def gray;           "\e[37m#{self}\e[0m" end
   
   def bg_black;       "\e[40m#{self}\e[0m" end
   def bg_red;         "\e[41m#{self}\e[0m" end
   def bg_green;       "\e[42m#{self}\e[0m" end
-  def bg_brown;       "\e[43m#{self}\e[0m" end
   def bg_blue;        "\e[44m#{self}\e[0m" end
   def bg_magenta;     "\e[45m#{self}\e[0m" end
   def bg_cyan;        "\e[46m#{self}\e[0m" end
   def bg_gray;        "\e[47m#{self}\e[0m" end
   
   def bold;           "\e[1m#{self}\e[22m" end
-  def italic;         "\e[3m#{self}\e[23m" end
-  def underline;      "\e[4m#{self}\e[24m" end
-  def blink;          "\e[5m#{self}\e[25m" end
   def reverse_color;  "\e[7m#{self}\e[27m" end
-  end
+end
 
 puts ""
 puts "Game Pieces:"
-print " 1 ".bg_red
+print "  1  ".bg_red.bold
 print " "
-print " 2 ".bg_green
+print "  2  ".bg_green.bold
 print " "
-print " 3 ".bg_blue
+print "  3  ".bg_blue.bold
 print " "
-print " 4 ".bg_magenta
+print "  4  ".bg_magenta.bold
 print " "
-print " 5 ".bg_cyan
+print "  5  ".bg_cyan.bold
 print " "
-print " 6 ".bg_gray
+print "  6  ".bg_gray.bold
 puts ""
 puts ""
 puts "Game Clues:"
-print " yes ".bg_black.red.bold
-print " "
-print " ??? ".bg_black.gray.bold
+print " * ".bg_black.gray
+print " Correct Color & Correct Spot"
+puts ""
+print " ? ".reverse_color
+print " Correct Color"
 puts ""
