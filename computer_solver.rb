5281b58d38db2bfc988fe6adea9fc351a728f41f
Rachel Moser <moser.rachel@gmail.com>
2020-06-21 19:07:08 -0500

    remove permutations with zero exact matches

--- computer_solver.rb
+++ computer_solver.rb
@@ -7,7 +7,7 @@ require './text_content'
 # class for code_maker game option
 class ComputerSolver
   attr_reader :maker_code, :computer_guess, :turn_count, :exact_number,
-              :same_number, :total_number
+              :same_number, :total_number, :code_tracker
 
   include GameLogic
   include Display
@@ -33,9 +33,11 @@ class ComputerSolver
     numbers = %w[1 2 3 4 5 6]
     options = numbers.shuffle
     @turn_count = 1
+    @code_tracker = []
     @computer_guess = find_four_numbers(options)
-    find_code_order(computer_guess) if exact_number.positive?
-    shuffle_guess(computer_guess) if exact_number.zero?
+    find_code_order(computer_guess)
+    # find_code_order(computer_guess) if exact_number.positive?
+    # shuffle_guess(computer_guess) if exact_number.zero?
   end
 
   def find_four_numbers(options, index = 0, guess = [])
@@ -54,45 +56,87 @@ class ComputerSolver
     show_code(guess)
     compare(master, guess)
     show_clues(exact_number, same_number)
+    current_guess = guess.clone
+    @code_tracker << [current_guess, exact_number, same_number]
   end
 
-  def shuffle_guess(array)
-    until exact_number.positive?
-      array.shuffle!
-      computer_turn(maker_code, array)
-      @turn_count += 1
-    end
-    find_code_order(array)
-  end
+  # def shuffle_guess(array)
+  #   until exact_number.positive?
+  #     array.shuffle!
+  #     computer_turn(maker_code, array)
+  #     @turn_count += 1
+  #   end
+  #   find_code_order(array)
+  # end
+
+  # def find_code_order(array)
+  #   create_permutations(array)
+  #   reduce_permutations(array)
+  #   code_order_guess(array)
+  #   computer_game_over(@code_permutations[0])
+  # end
 
   def find_code_order(array)
-    create_permutations(array)
-    reduce_permutations(array)
-    code_order_guess(array)
-    computer_game_over(@code_permutations[0])
+    @code_permutations = create_permutations(array)
+    @code_permutations.uniq!
+    # TEMPORARY - WILL DELETE THIS LINE AFTER THE CODE IS WORKING BUT NEED TO SEE IT
+    puts 'Previous Guesses'
+    @code_tracker.each_with_index { |code, index| puts "#{index}: #{code}" }
+    puts ''
+    puts 'Possible Combinations'
+    @code_permutations.each_with_index { |code, ind| puts "#{ind}: #{code}" }
+    compare_previous_guesses
   end
 
   def create_permutations(array)
-    @code_permutations = array.permutation.to_a
+    array.permutation.to_a
   end
 
-  def reduce_permutations(array)
-    @code_permutations.select! do |item|
-      compare(maker_code, item) >= compare(maker_code, array)
-    end
-    @code_permutations.uniq!
+  def compare_previous_guesses
+    remove_zero_exact
+    puts 'Permutations after remove_zero_exact'
+    @code_permutations.each_with_index { |code, ind| puts "#{ind}: #{code}" }
   end
 
-  def code_order_guess(array)
-    until @turn_count > 12 || solved?(maker_code, array)
-      @code_permutations.shift
-      computer_turn(maker_code, @code_permutations[0])
-      break if solved?(maker_code, @code_permutations[0])
+  def remove_zero_exact
+    @code_tracker.each do |code|
+      next unless code[1].zero? && code[2].positive?
 
-      reduce_permutations(@code_permutations[0])
-      @turn_count += 1
+      puts "remove_zero_exact for: #{code}"
+      examine_previous_guess(code[0])
     end
   end
+
+  def examine_previous_guess(code)
+    code.each_with_index do |number, index|
+      reject_permutations(number, index) if computer_guess.include?(number.to_s)
+    end
+  end
+
+  def reject_permutations(number, index)
+    puts "Removing number: #{number} from index: #{index}"
+    @code_permutations.reject! do |code|
+      code[index] == number.to_s
+    end
+  end
+
+  # def reduce_permutations(array)
+  #   @code_permutations.select! do |item|
+  #     compare(maker_code, item) >= compare(maker_code, array)
+  #   end
+  #   @code_permutations.uniq!
+  # end
+
+  # def code_order_guess(array)
+  #   until @turn_count > 12 || solved?(maker_code, array)
+  #     @code_permutations.shift
+  #     computer_turn(maker_code, @code_permutations[0])
+  #     break if solved?(maker_code, @code_permutations[0])
+
+  #     reduce_permutations(@code_permutations[0])
+  #     @turn_count += 1
+  #   end
+  # end
 
   def computer_game_over(guess)
     if solved?(maker_code, guess)
