9cda446dbe28a2a64e520598e8e19cc000446434
Rachel Moser <moser.rachel@gmail.com>
2020-06-23 02:20:25 +0000

    update code and peg colors

--- display.rb
+++ display.rb
@@ -4,19 +4,19 @@
 module Display
   def code_colors(number)
     {
-      '1' => "\e[44m  1  \e[0m ",
-      '2' => "\e[42m  2  \e[0m ",
-      '3' => "\e[45m  3  \e[0m ",
-      '4' => "\e[30;1m\e[46m  4  \e[0m ",
-      '5' => "\e[30;1m\e[43m  5  \e[0m ",
-      '6' => "\e[30;1m\e[41m  6  \e[0m "
+      '1' => "\e[101m  1  \e[0m ",
+      '2' => "\e[43m  2  \e[0m ",
+      '3' => "\e[44m  3  \e[0m ",
+      '4' => "\e[45m  4  \e[0m ",
+      '5' => "\e[46m  5  \e[0m ",
+      '6' => "\e[41m  6  \e[0m ",
     }[number]
   end
 
   def clue_colors(clue)
     {
-      '*' => "\e[31m\u2605\e[0m ",
-      '?' => "\e[38m\u2219\e[0m "
+      '*' => "\e[91m\u25CF\e[0m ",
+      '?' => "\e[37m\u25CB\e[0m "
     }[clue]
   end
 
